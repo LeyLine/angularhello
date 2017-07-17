93765101e987874b85652d6a6a4243257771691a
Adrian Llerena <a.llerena001@gmail.com>
2017-07-17 19:52:35 -0400

    Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app works';
+  content: string = 'A new world is waiting for you!'
 }
